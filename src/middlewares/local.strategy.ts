7d75a0bbd0fceceb29db3844c5ab5e0ebf89064b
tanthanh-cityboi <tanthanhe@gmail.com>
2022-12-01 14:02:26 +0700

    .

--- src/middlewares/local.strategy.ts
+++ src/middlewares/local.strategy.ts
@@ -16,6 +16,10 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(user_id: string, api_app_id: string): Promise<any> {
     const req: any = RequestContext.currentContext.req;
     const slackSignature = getSlackSignature(req);
+    console.log(
+      '🚀 ~ file: local.strategy.ts:19 ~ LocalStrategy ~ validate ~ slackSignature',
+      slackSignature,
+    );
     const payload = req.body;
     if (api_app_id !== process.env.APP_ID) {
       return {
@@ -26,6 +30,10 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     const requestTime = req.headers['x-slack-request-timestamp'];
     const secretKey = process.env.SECRET_KEY;
     const hashPayload = await hashSignature(requestTime, payload, secretKey);
+    console.log(
+      '🚀 ~ file: local.strategy.ts:33 ~ LocalStrategy ~ validate ~ hashPayload',
+      hashPayload,
+    );
     if (!isEqual(slackSignature, hashPayload)) {
       return {
         status: 400,
