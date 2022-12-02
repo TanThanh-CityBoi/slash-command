2bc10a741730de1a0afbc30022fd905256dfaa15
tanthanh-cityboi <tanthanhe@gmail.com>
2022-12-02 13:44:23 +0700

    .

--- src/response-template/bad-request.response.ts
+++ src/response-template/bad-request.response.ts
@@ -4,16 +4,16 @@ export function badRequestRes(data: any) {
   const { user_id, user_name, command, text } = body;
   const timeStamp = req.headers['x-slack-request-timestamp'];
 
-  const headerContent =
-    `*Command*: ${command} ${text}` +
-    `*CreatedBy*: <@${user_id}|${user_name}>` +
-    `*Time*: <!date^${timeStamp}^ {date_num} {time_secs}| 2014-02-18 6:39:42 AM PST>`;
+  const headerContent = `
+    *Command*: ${command} ${text}
+    *CreatedBy*: <@${user_id}|${user_name}>
+    *Time*: <!date^${timeStamp}^ {date_num} {time_secs}| 2014-02-18 6:39:42 AM PST>`;
 
-  const bodyContent =
-    `:star: :star: :star:` +
-    `*Status*: \`${status}\`` +
-    `*Message*: \`${message}\`` +
-    `*Errors*: \`${errors || null}\``;
+  const bodyContent = `
+    :star: :star: :star:
+    *Status*: \`${status}\`
+    *Message*: \`${message}\`
+    *Errors*: \`${errors || null}\``;
 
   return {
     blocks: [
