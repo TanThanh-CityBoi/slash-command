d9e6e451eb0c83ead19463b6ae919ab2f4a869ea
tanthanh-cityboi <tanthanhe@gmail.com>
2022-12-06 15:46:42 +0700

    .

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from 'src/middlewares';
 import { GithubController } from './github.controller';
 import { GithubService } from './github.service';
 
 @Module({
   imports: [PassportModule],
   controllers: [GithubController],
-  providers: [GithubService],
+  providers: [GithubService, LocalStrategy],
 })
 export class UserModule {}
